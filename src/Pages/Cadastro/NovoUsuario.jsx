69da9b4326870a11142be4a82a86a2b9e536f93f
Hiedi Ferreira <hiedyferreira@gmail.com>
2024-09-30 11:06:43 -0300

    feat(novoUsuario): Página de cadastro de novo usuário

--- src/Pages/Cadastro/NovoUsuario.jsx
+++ src/Pages/Cadastro/NovoUsuario.jsx
@@ -11,17 +11,27 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import theme from '../../Components/Temas/temaBotao'
 import { ThemeProvider } from '@mui/material/styles'
 
+import useCep from '../../Hooks/useCep.jsx'
+
 import styles from './novoUsuario.module.css'
 
 const formSchema = yup.object().shape({
     nomeUsuario: yup.string().max(50, 'Maximo de 50 caracteres').required('Nome é obrigatório'),
-    cpf: yup.string().required('CPF é obrigatório').matches(/^[0-9]{11}$/, 'Insira 11 dígitos (somente números').max(11, 'Insira 11 dígitos (somente números)'),    
+    cpf: yup.string().required('CPF é obrigatório').matches(/^[0-9]{11}$/, 'Insira 11 dígitos (somente números').max(11, 'Máximo 11 dígitos (somente números)'),    
     email: yup.string().email('Formato de e-mail inválido!').required('E-mail é obrigatório').max(50, 'Maximo de 50 caracteres'),   
     senha: yup.string().required('Senha é obrigatória').min(8, 'Minimo de 8 caracteres').max(16, 'Maximo de 16 caracteres'),
     dataNascimento: yup.string().required('Data de nascimento é obrigatória'),
+    cep: yup.string().required('CEP é obrigatório').matches(/^[0-9]{8}$/, 'Insira 8 dígitos (somente números)').max(8, 'Máximo 8 dígitos (somente números)'),
+    ruaUsuario: yup.string().required('Rua é obrigatória').max(50, 'Maximo de 50 caracteres'),
+    bairroUsuario: yup.string().required('Bairro é obrigatório').max(50, 'Maximo de 50 caracteres'),
+    numeroUsuario: yup.string().required('Número é obrigatório'),
+    complementoUsuario: yup.string().max(50, 'Maximo de 50 caracteres'),
+    cidadeUsuario: yup.string().required('Cidade é obrigatória').max(50, 'Maximo de 50 caracteres'),
+    estadoUsuario: yup.string().required('Estado é obrigatório').max(50, 'Maximo de 50 caracteres'),
 })
 
 function NovoUsuario(){
+    let dadosCep = {}
 
     //Função para mostrar ou ocultar a senha
     const [showPassword, setShowPassword] = useState(false);
@@ -30,6 +40,8 @@ function NovoUsuario(){
     const {
         register,
         handleSubmit,
+        getValues,
+        setValue,
         formState: { errors }
     } = useForm({
         resolver: yupResolver(formSchema)
@@ -37,9 +49,22 @@ function NovoUsuario(){
 
     async function formularioCadastro(dadosForm){
         console.log(dadosForm)
+        //Aqui vamos chamar cadastrarUsuario(dadosForm)
     }
 
     //Buscar Cep
+    const buscarCep = async () => {
+        let cep = getValues('cep')
+        dadosCep = (await useCep(cep)).data
+        if(dadosCep.erro){
+            alert('Cep inválido')
+        } else {
+            setValue('ruaUsuario', dadosCep.logradouro)
+            setValue('bairroUsuario', dadosCep.bairro)
+            setValue('cidadeUsuario', dadosCep.localidade)
+            setValue('estadoUsuario', dadosCep.uf)
+        }
+    }
 
     return(
         <div>
@@ -71,7 +96,7 @@ function NovoUsuario(){
                     <input  type="date" name="dataNascimento" format='DD-MM-YYYY'
                         {...register("dataNascimento")}                                                   
                     />
-                    {errors?.data && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.data.message}</p>}
+                    {errors?.dataNascimento && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.dataNascimento.message}</p>}
 
                     <label htmlFor="email">E-mail</label>
                     <input  type="email" placeholder="Informe o e-mail" name="email"
@@ -82,7 +107,7 @@ function NovoUsuario(){
                     <label htmlFor="senha">Senha</label>
                     <div className={styles.containerSenha}>
                         <input type={showPassword ? 'text' : 'password'}
-                        placeholder="Informe uma senha"
+                        placeholder="Informe uma senha" name='senha'
                             {...register("senha")}
                         />
 
@@ -92,9 +117,59 @@ function NovoUsuario(){
                     </div>
                     {errors?.senha && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.senha.message}</p>}
 
-                    <div className={styles.botaoLogin}>
+                    <label htmlFor="cep">CEP</label>
+                    <input  type="text" placeholder="Informe o CEP (somente dígitos)" name="cep"
+                        {...register("cep", {
+                            onBlur: () => buscarCep()
+                        })}                                                   
+                    />
+                    {errors?.cep && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.cep.message}</p>}
+
+                    <label htmlFor="ruaUsuario">Rua</label>
+                    <input  type="text" placeholder="Informe a rua" name="ruaUsuario"
+                        {...register("ruaUsuario")}                                                   
+                    />
+                    {errors?.ruaUsuario && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.ruaUsuario.message}</p>}
+
+                    <label htmlFor="bairroUsuario">Bairro</label>
+                    <input  type="text" placeholder="Informe o bairro" 
+                        {...register("bairroUsuario")}                                                   
+                    />
+                    {errors?.bairroUsuario && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.bairroUsuario.message}</p>}
+
+                    <label htmlFor="numeroUsuario">Número</label>
+                    <input  type="number" placeholder="Informe o número" min={0}
+                        {...register("numeroUsuario")}                                                   
+                    />
+                    {errors?.numeroUsuario && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.numeroUsuario.message}</p>}
+
+                    <label htmlFor="complementoUsuario">Complemento</label>
+                    <input  type="text" placeholder="Complemento (se houver)" 
+                        {...register("complementoUsuario")}                                                   
+                    />
+                    {errors?.complementoUsuario && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.complementoUsuario.message}</p>}
+
+                    <label htmlFor="cidadeUsuario">Cidade</label>
+                    <input  type="text" placeholder="Informe a cidade" 
+                        {...register("cidadeUsuario", {
+                            required: "Campo obrigatório!",
+                            maxLength: {value:50, message:"Deve possuir no máximo 50 caracteres"}
+                        })}                                                   
+                    />
+                    {errors?.cidadeUsuario && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.cidadeUsuario.message}</p>}
+
+                    <label htmlFor="estadoUsuario">Estado</label>
+                    <input  type="text" placeholder="Informe o estado" 
+                        {...register("estadoUsuario", {
+                            required: "Campo obrigatório!",
+                        })}                                                   
+                    />
+                    {errors?.estadoUsuario && <p className={styles.msgErro}><WarningAmberIcon fontSize="small" sx={{"mr":1}}/>{errors.estadoUsuario.message}</p>}
+                    
+                    <div className={styles.botaoCadastrar}>
                         <ThemeProvider theme={theme}>
-                            <Button                                
+                            <Button      
+                                onClick={handleSubmit(formularioCadastro)}                          
                                 type="submit"
                                 variant="contained"
                                 color="primary"
